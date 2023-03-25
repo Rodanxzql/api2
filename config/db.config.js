ce382e837abf33fae08dbdb0caf35f0f3709fef2
Rodanxzql <92496910+Rodanxzql@users.noreply.github.com>
2023-03-25 07:39:11 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: "bgenshin.cmywhljzxxnf.ap-southeast-1.rds.amazonaws.com:3306",
+    HOST: "bgenshin.cmywhljzxxnf.ap-southeast-1.rds.amazonaws.com",
     USER: "admin",
     PASSWORD: "01682998982",
     DB: "laravel"
